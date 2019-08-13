c23c3c4d1196f51c8a6b8a6313353e332ef3db43
DustinGaudet <dustin@dustingaudet.com>
2019-08-13 07:17:38 -0400

    Rough wireframe Checkout component.

---
+++ src/components/Checkout.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Checkout.css';
+
+const CartItem = () => (
+    <div className="CartItem">
+        <div className="CartItem-img"></div>
+        <h4>Salmon w/ Rice, Potatoes, &amp; ...</h4>
+        <p>Large</p>
+        <p>$7.99</p>
+    </div>
+);
+
+const Cart = () => (
+    <div className="Cart">
+        <CartItem />
+        <CartItem />
+        <CartItem />
+    </div>
+);
+
+const Checkout = () => (
+    <div className="Checkout">
+        <h2>Checkout</h2>
+        <Cart />
+        <div className="total">
+            Total $22.97
+        </div>
+        <p>*Meals are delivered every Sunday. The next delivery date is Sunday, July 28. Order by 11:59pm Friday, July 26 to receive your delivery this Sunday.</p>
+        <div className="Stripe">
+            <input placeholder="Address" />
+            <input placeholder="City" />
+            <input placeholder="Province" />
+            <input placeholder="Postal Code" />
+            <input placeholder="Card number" />
+            <input placeholder="Expiration" />
+            <input placeholder="CVC" />
+        </div>
+        <button>Pay $22.97</button>
+    </div>
+);
+
+export default Checkout;
