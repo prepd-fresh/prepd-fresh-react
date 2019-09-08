19ec4533f49368a4fe4b33e5d1c6d14565b26726
DustinGaudet <dustin@dustingaudet.com>
2019-09-08 19:01:04 -0400

    Implement basic form validation with Formik & Yup

--- src/components/Checkout/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm.js
@@ -1,38 +1,109 @@
-import React, {Component} from 'react';
-import {CardElement, injectStripe} from 'react-stripe-elements';
+import React, { useState } from 'react';
+import * as yup from 'yup';
+import { Formik, Form, Field } from 'formik';
+import { CardElement, injectStripe } from 'react-stripe-elements';
 
-class CheckoutForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { complete: false }
-    this.submit = this.submit.bind(this);
-  }
+const checkoutValidationSchema = yup.object().shape({
+    firstName: yup.string()
+        .required('Required')
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!'),
+    lastName: yup.string()
+        .required('Required')
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!'),
+    address: yup.string()
+        .required('Required')
+        .matches(
+            /^[\d]{1,}[a-zA-Z\s]{1,}$/,
+            'Invalid address format.'
+        ),
+    phoneNumber: yup.string()
+        .required('Required')
+        .matches(
+            /^\d{3}-\d{3}-\d{4}$/, 
+            'Phone number must have the format "647-123-4567"'
+        ),
+    email: yup.string()
+        .email('Invalid email')
+        .required('Required'),
+})
 
-  async submit(ev) {
-    let {token} = await this.props.stripe.createToken({name: "Name"});
-    let response = await fetch("/charge", {
-      method: "POST",
-      headers: {"Content-Type": "text/plain"},
-      body: token.id
-    });
-  
-    if (response.ok) this.setState({complete: true});
-  }
-  
+const CheckoutForm = (props) => {
+  const [complete, setComplete] = useState(false);
 
-  render() {
-    return (this.state.complete) ? 
-    (
-      <h1>Purchase Complete</h1>
-    ) :
-    (
+  return (complete) 
+    ? <h1>Purchase Complete</h1>
+    : (
       <div className="checkout">
-        <p>Would you like to complete the purchase?</p>
-        <CardElement />
-        <button onClick={this.submit}>Send</button>
+        <Formik 
+          initialValues={{
+              firstName: "",
+              lastName: "",
+              address: "",
+              phoneNumber: "",
+              email: "",
+          }}
+          validationSchema={checkoutValidationSchema}
+          onSubmit={async ev => {
+              let {token} = await props.stripe.createToken({
+                  name: "Name"
+              });
+              let response = await fetch(
+                  "/charge", 
+                  {
+                      method: "POST",
+                      headers: { "Content-Type": "text/plain" },
+                      body: token.id
+                  }
+              );
+              if (response.ok) setComplete(true);
+          }}>
+          {({ errors, touched }) => (
+              <Form>
+                <p>Would you like to finish your purchase?</p>
+                <CardElement />
+                <Field 
+                    name="firstName" 
+                    placeholder="firstName"    
+                />
+                {(errors.firstName && touched.firstName)
+                    ? <div>{errors.firstName}</div>
+                    : null}
+                <Field 
+                    name="lastName" 
+                    placeholder="lastName"    
+                />
+                {(errors.lastName && touched.lastName)
+                    ? <div>{errors.lastName}</div>
+                    : null}
+                <Field 
+                    name="address" 
+                    placeholder="address"    
+                />
+                {(errors.address && touched.address)
+                    ? <div>{errors.address}</div>
+                    : null}
+                <Field 
+                    name="phoneNumber" 
+                    placeholder="phoneNumber"    
+                />
+                {(errors.phoneNumber && touched.phoneNumber)
+                    ? <div>{errors.phoneNumber}</div>
+                    : null}
+                <Field 
+                    name="email" 
+                    placeholder="email"    
+                />
+                {(errors.email && touched.email)
+                    ? <div>{errors.email}</div>
+                    : null}
+                <br/><button type="submit">Pay $</button>
+              </Form>
+            )}
+        </Formik>
       </div>
     );
-  }
 }
 
 export default injectStripe(CheckoutForm);
