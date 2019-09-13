28fa97c8730ef24f1e4a1a4fe40c98822d0641ff
DustinGaudet <dustin@dustingaudet.com>
2019-09-13 19:13:03 -0400

    Update App component to use 'styled'

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actions from '../actions';
-import './App.css';
 import Home from './Home/Home';
 import NavMenu from './NavMenu';
 import Footer from './Footer';
 import Menu from './Menu/Menu';
 import Checkout from './Checkout/Checkout';
+import styled from 'styled-components';
 
 const App = () => {
   const state = useSelector(state => state);
@@ -34,4 +34,44 @@ const App = () => {
   );
 }
 
-export default App;
+export default styled(App)`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #FAF8F4; 
+    min-height: 100vh;
+  }
+
+  main {
+    position: relative;
+  }
+
+  main > * {
+    width: 320px;
+    border: 1px solid red;
+    margin: auto;
+    height: auto;
+  }
+
+  .NavMenu {
+    height: 40px;
+    border: 1px solid blue;
+  }
+
+  @media screen and (min-width: 1024px) {
+    main > *{
+      width: 1024px;
+    }
+
+    .NavMenu {
+      height: 60px;
+    }
+  }
+
+  .Footer {
+    height: 40px;
+    border: 1px solid blue;
+  }
+`;
