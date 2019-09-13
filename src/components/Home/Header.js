2f4158e3fe63bb25b0482e20e81ee0424b034bfa
DustinGaudet <dustin@dustingaudet.com>
2019-09-13 19:11:59 -0400

    Make ORDER NOW link to menu view

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = props => (
     <header style={{backgroundImage: `url('/img/${props.heroImageUrl}')`}}>
         <h1>Student life made easy</h1>
-        <button>ORDER NOW</button>
+        <Link to="/menu/">ORDER NOW</Link>&nbsp;
         <p>New menu weekly | No tax | Free delivery</p>
     </header>
 );
