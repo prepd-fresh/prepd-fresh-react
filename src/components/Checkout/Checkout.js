504391c12f3df9c3b484b36f7feeb7d59a8e58df
DustinGaudet <dustin@dustingaudet.com>
2019-09-13 20:19:27 -0400

    Replace checkout stylesheet with styled-components

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm';
-import './Checkout.css';
+import styled from 'styled-components';
 import Cart from './Cart';
 
-const Checkout = ({cartIsVisible}) => {
+const Checkout = ({cartIsVisible, className}) => {
     const cartItems = useSelector(state => ({...state.cart}))
     const totalPrice = Object.keys(cartItems).reduce(
         (total, itemId) => (
@@ -16,7 +16,7 @@ const Checkout = ({cartIsVisible}) => {
     return (
         // This is the test private for Dustin's Stripe account. TODO: Change to Ben's
         <StripeProvider apiKey="pk_test_dxJRiJo1wDpI8NWpKyTy9WII00GF5Wl5rQ">
-            <div className="Checkout" 
+            <div className={className} 
                  style={{display: cartIsVisible ? "block" : "none"}}>
                 <h2>Checkout</h2>
                 <Cart cartItems={cartItems} />
@@ -34,4 +34,67 @@ const Checkout = ({cartIsVisible}) => {
     );
 }
 
-export default Checkout;
+export default styled(Checkout)`
+    & {
+        width: 320px;
+        border: 1px solid blue;
+        box-sizing: border-box;
+        padding: 20px;
+        position: absolute;
+        top: 0;
+        right: 0;
+        background-color: #FAF8F4;
+        height: 100%;
+    }
+
+    & h2 {
+        margin-top: 0;
+    }
+
+    .Cart {
+        border: 1px solid green;
+        padding: 0 10px;
+    }
+
+    .CartItem {
+        display: grid;
+        grid-template: 30px / 4fr 1fr 1fr; 
+        padding: 10px;
+        border-bottom: 1px solid gray;
+    }
+
+    .CartItem:last-child {
+        display: grid;
+        grid-template: 30px / 4fr 1fr 1fr; 
+        padding: 10px;
+        border-bottom: none;
+    }
+
+    .CartItem .delete-btn {
+        text-align: right;
+    }
+
+    .CartItem h4,
+    .CartItem p {
+        font-size: 9px;
+    }
+
+    .CartItem .item-name-and-size h4 {
+        margin: 0;
+    }
+
+    .CartItem .item-name-and-size p {
+        font-size: 11px;
+        margin: 0;
+    }
+
+    .total {
+        border: 1px solid green;
+        margin-top: 15px;
+        padding: 10px;
+    }
+
+    @media screen and (min-width: 1024px) {
+        /* show delete buttons instead of slideable cart items */
+    }
+`;
